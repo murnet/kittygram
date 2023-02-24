d42f7f9f293015688af2d1bc441f491187ccea47
Maksim Arkhipov <manjanfe@gmail.com>
2023-02-24 15:48:07 +0700

    Add view-function for path,put,delete.

--- kittygram/urls.py
+++ kittygram/urls.py
@@ -1,9 +1,11 @@
 from django.urls import include, path
 
-from cats.views import cat_list
+# from cats.views import cat_list
+from cats import views
 
 urlpatterns = [
-   path('cats/', cat_list),
+   path('cats/', views.cat_list),
+   path('cats/<int:pk>/', views.cat_detail),
 ]
 
 
